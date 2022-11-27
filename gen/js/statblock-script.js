d3486856d3097b6b16dfd85842ed437a8d2b9282
Tuomari-ua <osya.lia@gmail.com>
2022-11-27 10:21:53 +0200

    Update statblock-script.js

--- gen/js/statblock-script.js
+++ gen/js/statblock-script.js
@@ -1974,27 +1974,27 @@ var ArrayFunctions = {
 // Document ready function
 $(function () {
     // Load the preset monster names
-    $.getJSON("https://api.open5e.com/monsters/?format=json&fields=slug,name&limit=1000&document__slug=wotc-srd", function (srdArr) {
-        let monsterSelect = $("#monster-select");
-        monsterSelect.append("<option value=''></option>");
-        monsterSelect.append("<option value=''>-5e SRD-</option>");
-        $.each(srdArr.results, function (index, value) {
-            monsterSelect.append("<option value='" + value.slug + "'>" + value.name + "</option>");
-        })
-        $.getJSON("https://api.open5e.com/monsters/?format=json&fields=slug,name&limit=1000&document__slug=tob", function (tobArr) {
-            monsterSelect.append("<option value=''></option>");
-            monsterSelect.append("<option value=''>-Tome of Beasts (Kobold Press)-</option>");
-            $.each(tobArr.results, function (index, value) {
-                monsterSelect.append("<option value='" + value.slug + "'>" + value.name + "</option>");
-            })
-        })
-            .fail(function () {
-                $("#monster-select-form").html("Unable to load Tome of Beasts monster presets.")
-            });
-    })
-        .fail(function () {
-            $("#monster-select-form").html("Unable to load monster presets.")
-        });
+ //   $.getJSON("https://api.open5e.com/monsters/?format=json&fields=slug,name&limit=1000&document__slug=wotc-srd", function (srdArr) {
+ //       let monsterSelect = $("#monster-select");
+ //       monsterSelect.append("<option value=''></option>");
+ //       monsterSelect.append("<option value=''>-5e SRD-</option>");
+ //       $.each(srdArr.results, function (index, value) {
+ //           monsterSelect.append("<option value='" + value.slug + "'>" + value.name + "</option>");
+ //       })
+ //       $.getJSON("https://api.open5e.com/monsters/?format=json&fields=slug,name&limit=1000&document__slug=tob", function (tobArr) {
+ //           monsterSelect.append("<option value=''></option>");
+ //           monsterSelect.append("<option value=''>-Tome of Beasts (Kobold Press)-</option>");
+ //           $.each(tobArr.results, function (index, value) {
+ //               monsterSelect.append("<option value='" + value.slug + "'>" + value.name + "</option>");
+  //          })
+    //    })
+      //      .fail(function () {
+        //        $("#monster-select-form").html("Unable to load Tome of Beasts monster presets.")
+          //  });
+    //})
+      //  .fail(function () {
+        //    $("#monster-select-form").html("Unable to load monster presets.")
+       // });
 
     // Load the json data
     $.getJSON("js/JSON/statblockdata.json", function (json) {
