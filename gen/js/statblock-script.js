e83c7a91cc5f4e7a64fdd9e2e474bec47ffff53a
Tuomari-ua <osya.lia@gmail.com>
2022-12-06 19:56:12 +0200

    Update statblock-script.js

--- gen/js/statblock-script.js
+++ gen/js/statblock-script.js
@@ -444,7 +444,7 @@ function BuildMarkdown(isV3Markdown) {
         `## ${mon.name}`,
         //`*${StringFunctions.StringCapitalize(data.sizes[mon.size].localizedName)} ${mon.type}${mon.tag != "" ? ` (${mon.tag})`  : ""}, ${mon.alignment}*`,
         //`___`,
-		`*${StringFunctions.StringCapitalize(mon.type $ {mon.tag !="" ? ` (${mon.tag})` : ""} ${(data.sizes[mon.size].localizedName) : "розміру"}, ${mon.alignment}*`,
+		`*${StringFunctions.StringCapitalize({mon.type}${mon.tag != "" ? ` (${mon.tag})`  : ""}) $(data.sizes[mon.size].localizedName)} ${mon.alignment}*`,
 	`___`,
         PrintMarkdownProperty(isV3Markdown, "Клас захисту", StringFunctions.FormatString(StringFunctions.GetArmorData())),
         PrintMarkdownProperty(isV3Markdown, "Пункти здоров'я", StringFunctions.GetHP()), 
